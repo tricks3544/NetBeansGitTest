30b4e6cf81ce9c448a9ed6bf87f67cfa4efb8554
tricks3544 <timri@10.80.29.145>
2020-02-11 15:05:31 -0600

    Initial Commit

---
+++ OneDrive/Documents/NetBeansProjects/NetBeansGitProject1/src/mcis/jsu/TestClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mcis.jsu;
+
+/**
+ *
+ * @author timri
+ */
+public class TestClass {
+    public static String testMethod1() {
+        return "Hello World";
+    }
+    
+}
